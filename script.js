99252a5377fd3ef1629be7fafed58f06c3f75374
HarshDex <nature4917@gmail.com>
2024-03-06 11:15:23 +0530

    added files

---
+++ script.js
@@ -0,0 +1,130 @@
+// cursor code starts
+gsap.registerPlugin(ScrollTrigger);
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, 
+    getBoundingClientRect() {
+      return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+    },
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+  });
+
+
+// function manageLocomotive() {
+//   if (window.innerWidth <= 768) { 
+//     if (scroll.destroyed) return;  
+//     scroll.destroy();  
+//   } else {
+//     if (!scroll.destroyed) return;
+//     scroll.init(); 
+//   }
+// }
+
+// window.addEventListener('resize', manageLocomotive); 
+// manageLocomotive();
+
+
+ScrollTrigger.defaults = {
+  scroller : '.main',
+}
+
+const video = document.querySelector('.page2 video');
+const cursor = document.querySelector('.cursor');
+
+
+const commonScrollTriggerConfig = {
+    trigger: '.page2',
+    scroller : '.main',
+    start: '-10% 70%',
+    end: 'center 70%',
+    scrub: true,
+    ease : 'expo.inOut',};
+
+gsap.timeline()
+    .to('.page2-video', { 
+        width: '95vw', 
+        scrollTrigger: commonScrollTriggerConfig 
+    })
+    .to('.navigation', { 
+        height: '7vh', 
+        scrollTrigger: { 
+            trigger: '.page2',
+            scroller : '.main',
+            start: '10% 10%', 
+            end: 'center center',
+            delay : .5, 
+            ease :'expo.out',
+            scrub: true, 
+    }})
+
+gsap.utils.toArray(['.page2','.page3']).forEach((pannel)=>{
+    ScrollTrigger.create({
+        trigger : pannel,
+        scroller : '.main',
+        start : 'top top',
+        end: '+=70%',
+        scrub : true,
+        pinSpacing : false,
+        pin : true,
+        // markers : true,
+    })
+})
+
+const page3custom ={
+    trigger : ".page3",
+    scroller : '.main',
+    start : 'top 80%',
+    end : 'top top',
+    scrub : true,
+    duration : 4,
+    ease : 'back.inOut(1.8)',
+}
+
+gsap.timeline()
+    .to('.navigation',{
+    opacity : 0,
+    scrollTrigger : page3custom,    
+  })
+  .to('.page3',{
+    backgroundColor : 'white',
+    duration : 5,
+    scrollTrigger : page3custom,    
+    snap : 1/4,
+  })
+
+
+// lerping effect
+// const lerpTextElements = document.querySelectorAll('.page3-info-section p');
+
+// scroll.on('scroll', (obj) => {
+//   lerpTextElements.forEach(textElement => {
+//     const progress = obj.scroll.y / (obj.limit.y - window.innerHeight); 
+//     const newY = progress * 100; // Move the text 100 units as you scroll the entire height 
+//     gsap.to(textElement, { 
+//       y: newY, 
+//       duration: 1, // Adjust the animation duration
+//       ease: 'power2.out' 
+//     });
+//   })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
+  
